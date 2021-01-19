62f92ab031add86fd6f7b9395774f62ba786fd77
Rendy Artha <rendyartha312@gmail.com>
2021-01-19 15:03:43 +0700

    finish

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,22 +1,41 @@
 import React, {useState, useEffect} from 'react'
-import rgbToHex from './utils';
 
 const SingleColor = ({ rgb, weight, index, hexColor }) => {
   const [alert, setAlert] = useState(false);
+
   const bcg = rgb.join(',')
+  const hexValue = `#${hexColor.toUpperCase()}`
+
+  const copyClipboard = () => {
+    setAlert(true)
+    navigator.clipboard.writeText(hexValue)
+  }
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false)
+    }, 1000)
+    return () => clearTimeout(timeout)
+  }, [alert])
 
   return (
     <div className="">
       <div
         className={`color ${index > 10 && 'color-light'}`}
         style={{backgroundColor: `rgb(${bcg})`}}
+        onClick={copyClipboard}
       >
         <p>
           {weight}%
         </p>
         <p>
-          {hexColor.toUpperCase()}
+          {hexValue}
         </p>
+        {alert && 
+          <p className="font-medium text-gray-300 text-xs w-full text-center pt-8">
+            COPIED TO CLIPBOARD
+          </p>
+        }
       </div>
     </div>
   )
